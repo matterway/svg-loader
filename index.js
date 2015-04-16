dd36623801da971abe61d53badbb1857a25e5398
Omar Kamali <okamali@productivemobile.com>
2015-04-16 14:10:38 +0200

    Support for escape and data-uri parameters

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
+var loaderUtils = require("loader-utils");
+
 module.exports = function(content) {
   this.cacheable();
-  return "module.exports = 'data:image/svg+xml;charset=utf8," + encodeURIComponent(content.toString()) + "'";
+  var svgData = content.toString();
+  var query = loaderUtils.parseQuery(this.query);
+  if (query.escape) {
+    svgData = encodeURIComponent(content.toString());
+  }
+  if (query.uri) {
+    svgData = "data:image/svg+xml;charset=utf8," + svgData;
+  }
+  svgData = "'" + svgData + "'";
+  return "module.exports = " + svgData;
 }
 module.exports.raw = true;
 
